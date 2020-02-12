f21e2ff3cc8f9a0d28f049f6f797c766d7f6f913
AugustoHepp <gutoha@gmail.com>
2020-02-12 14:46:58 -0300

    Inclusao do bootstrap

--- resources/views/ResultadoBusca.blade.php
+++ resources/views/ResultadoBusca.blade.php
@@ -3,14 +3,15 @@
 
 @section('conteudo')
     <h1>Artigos pesquisados</h1>
-    <a href="{{ route('index') }}">Nova busca</a>
-
+    <h2>
+        <a href="{{ route('index') }}">Nova busca</a>
+    </h2>
     <table class="table table-striped">
         <thead>
             <tr>
                 <th>Artigo</th>
                 <th>Link</th>
-                <th width="100">Excluir</th>
+                <th width="100">Ação</th>
             </tr>
         </thead>
         <tbody>
@@ -19,7 +20,7 @@
                     <td>{{ $artigo->titulo }}</td>
                     <td><a href="{{ $artigo->link }}">{{ $artigo->link }}</a></td>
                     <td>
-                        <a href="{{ route('artigos.delete', $artigo->id) }}">Excluir</a>
+                        <a href="{{ route('artigos.delete', $artigo->id) }}" class="btn btn-danger">Excluir</a>
                     </td>
                 </tr>
             @endforeach
